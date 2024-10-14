1bd6dec2dd9e9d66036965d81e6cfe55bd54aa65
Dipak <125576159+DipakShinde1196@users.noreply.github.com>
2024-10-14 22:11:56 +0530

    Update signals.py

--- myapp/signals.py
+++ myapp/signals.py
@@ -1,12 +1,9 @@
-# signals.py
 from django.dispatch import Signal, receiver
 from django.db import transaction
 from myapp.models import MyModel
 
-# Define a custom signal
 my_signal = Signal()
 
-# Define a receiver function that modifies the database
 @receiver(my_signal)
 def my_receiver(sender, **kwargs):
     MyModel.objects.create(name="Signal Entry")
